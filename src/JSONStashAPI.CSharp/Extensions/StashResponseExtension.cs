a40ec33857ad66489619a382a3e60bf5fd25a2dd
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-05-07 21:47:36 -0500

    Added quality of like extension for converting returned json to object.

---
+++ src/JSONStashAPI.CSharp/Extensions/StashResponseExtension.cs
@@ -0,0 +1,30 @@
+﻿using JSONStashAPI.CSharp.Models;
+using Newtonsoft.Json;
+
+namespace JSONStashAPI.CSharp.Extensions
+{
+    public static class StashResponseExtension
+    {
+        /// <summary>
+        /// Converts the specified string containing the representation of a object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static bool TryParseData<T>(this StashResponse response, out T result)
+        {
+            bool canDeserialize = true;
+            
+            JsonSerializerSettings settings = new()
+            {
+                Error = (sender, args) => { canDeserialize = false; args.ErrorContext.Handled = true; },
+                MissingMemberHandling = MissingMemberHandling.Error
+            };
+
+            result = JsonConvert.DeserializeObject<T>(response.Data, settings);
+            
+            return canDeserialize;
+        }
+    }
+}
